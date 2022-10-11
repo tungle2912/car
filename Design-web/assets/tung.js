cbbf504499b532ccab68d7c684a648aeeb619636
xuan.le <xuan.le@amela.vn>
2022-10-11 16:16:15 +0700

    tunglevan

--- Design-web/assets/tung.js
+++ Design-web/assets/tung.js
@@ -56,11 +56,73 @@ modalClode.addEventListener('click',closeBtnSearch)
 // menuOpen.addEventListener('click',showMenu)
 // menuClose.addEventListener('click',closeMenu)
 
+const menuOpen=document.querySelector('.js-menu');
+    const menu=document.querySelector('.js-menu-modal');
+    const menuClose=document.querySelector('.js-close');
+
+
+    function showMenu(){
+    menu.classList.add('open');
+    }
+    function closeMenu(){
+    menu.classList.remove('open');
+    }
+
+         menuOpen.addEventListener('click',showMenu)
+         menuClose.addEventListener('click',closeMenu)
+
+
+    //open and close login
+    const login = document.querySelector('.js-login');
+    const register=document.querySelector('.js-register');
+    const account = document.querySelector('.js-account');
+    const closeLogin = document.querySelector('.js-login-close');
+        function showLogin(){
+        account.classList.add('open');
+         }
+         function loginClose(){
+        account.classList.remove('open');
+         }
+
+
+    register.addEventListener('click',closeMenu);
+    login.addEventListener('click',showLogin);
+    login.addEventListener('click',closeMenu);
+    closeLogin.addEventListener('click',loginClose);
+
 
 
 
  
+//login and register
+    const options=document.querySelectorAll('.changetype');
+    const form=document.getElementById("form");
+    const bgactive=document.getElementById("bg-active");
+    const registerid=document.getElementById("register");
 
+    for(const option of options){
+        option.addEventListener('click',function(event){
+            
+            //   form.classList.remove('login');
+            form.classList.remove('register');
+            form.classList.add(this.id);
+            bgactive.style.left=this.offsetLeft+'px'
+            for(const op of options){
+                op.classList.remove('active');
+
+            }
+            this.classList.add('active')
+        })
+    }
+    function removelogin(){
+        form.classList.remove('login');
+        form.classList.add('register');
+        account.classList.add('open');
+        bgactive.style.left=registerid.offsetLeft+'px'
+
+    }
+
+    register.addEventListener('click',removelogin)
 
 
 
