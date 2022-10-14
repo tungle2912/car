01cb24d7b969efab476c206a73695666faf4f57d
xuan.le <xuan.le@amela.vn>
2022-10-14 23:01:58 +0700

    hihi

--- Design-web/assets/tung.js
+++ Design-web/assets/tung.js
@@ -40,21 +40,7 @@ for(const searchBtn of searchBtns){
 modalClode.addEventListener('click',closeBtnSearch)
 
 
-// open and close menu
-// const menuOpen=document.querySelector('.js-menu');
-// const menu=document.querySelector('.js-menu-modal');
-// const menuClose=document.querySelector('.js-close');
 
-
-// function showMenu(){
-//     menu.classList.add('open');
-// }
-// function closeMenu(){
-//     menu.classList.remove('open');
-// }
-
-// menuOpen.addEventListener('click',showMenu)
-// menuClose.addEventListener('click',closeMenu)
 
 const menuOpen=document.querySelector('.js-menu');
     const menu=document.querySelector('.js-menu-modal');
@@ -76,19 +62,30 @@ const menuOpen=document.querySelector('.js-menu');
     const login = document.querySelector('.js-login');
     const register=document.querySelector('.js-register');
     const account = document.querySelector('.js-account');
+    const logincontainer = document.querySelector('.js-account__container')  
     const closeLogin = document.querySelector('.js-login-close');
-        function showLogin(){
+        function showaccount(){
         account.classList.add('open');
          }
          function loginClose(){
         account.classList.remove('open');
+        logincontainer.classList.remove('open'); 
          }
+        function showLogin(){
+        logincontainer.classList.add('open');
+        }
 
 
     register.addEventListener('click',closeMenu);
-    login.addEventListener('click',showLogin);
+    login.addEventListener('click',showaccount);
     login.addEventListener('click',closeMenu);
+    login.addEventListener('click',showLogin);
     closeLogin.addEventListener('click',loginClose);
+    
+
+
+
+
 
 
 
@@ -99,11 +96,12 @@ const menuOpen=document.querySelector('.js-menu');
     const form=document.getElementById("form");
     const bgactive=document.getElementById("bg-active");
     const registerid=document.getElementById("register");
+    const loginid=document.getElementById("login");
 
     for(const option of options){
         option.addEventListener('click',function(event){
             
-            //   form.classList.remove('login');
+              form.classList.remove('login');
             form.classList.remove('register');
             form.classList.add(this.id);
             bgactive.style.left=this.offsetLeft+'px'
@@ -118,10 +116,16 @@ const menuOpen=document.querySelector('.js-menu');
         form.classList.remove('login');
         form.classList.add('register');
         account.classList.add('open');
+        logincontainer.classList.add('open');
         bgactive.style.left=registerid.offsetLeft+'px'
-
     }
 
+    login.addEventListener('click',function(e){
+        form.classList.add('login');
+        form.classList.remove('register');
+        bgactive.style.left=loginid.offsetLeft+'px'
+
+    })
     register.addEventListener('click',removelogin)
 
 
@@ -137,4 +141,38 @@ window.onscroll = function() {
         goToTopBtn.classList.remove('active');
     }
 };
+
+
+// click img
+const closeInfor=document.querySelector('.js-infor-close');
+const imgs= document.querySelectorAll('.product_item')
+    for(const img of imgs){
+        img.addEventListener('click',showaccount);
+        img.addEventListener('click', function(event){
+            const t=this.id;
+            const t2=document.getElementsByClassName(t)
+            t2[0].classList.add('open');
+        });
+    };
+const infors=document.querySelectorAll('.infor__list');
+const closeInfors=document.querySelectorAll('.js-infor-close');
+
+for(const infor of infors){
+    for(const closeInfor of closeInfors){
+        closeInfor.addEventListener('click', function(e){
+            infor.classList.remove('open');
+            account.classList.remove('open');
+        })
+
+
+    }
+}
+ 
+
+// console.log(t);
+// closeInfor.addEventListener('click', function(ev) {
+//     const t3=document.getElementsByClassName(t)
+//     t3[0].classList.remove('open');
+
+
 
